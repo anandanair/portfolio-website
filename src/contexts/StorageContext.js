f01763ae682e02ad157c69ff7c9ea561eeb5e319
Anand <anandanairaan@gmail.com>
2023-01-14 17:42:22 +0800

    upload image fnc

--- src/contexts/StorageContext.js
+++ src/contexts/StorageContext.js
@@ -1,7 +1,9 @@
 import { Box, CircularProgress } from "@mui/material";
-import { getDownloadURL, ref } from "firebase/storage";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import React, { useContext, useEffect, useState } from "react";
 import { storage } from "../firebase";
+import { useFirestore } from "./FirestoreContext";
+import { useAuth } from "./AuthContext";
 
 const StorageContext = React.createContext();
 
@@ -12,20 +14,35 @@ export function useStorage() {
 export function StorageProvider({ children }) {
   const [defaultPhotoURL, setDefaultPhotoURL] = useState("");
   const [loading, setLoading] = useState(true);
+  const { currentUser } = useAuth();
+  const { updatePortfolio } = useFirestore();
 
-  async function getDefaultImageURL(imageName) {
-    return await getDownloadURL(ref(storage, `default/images/${imageName}`));
+  //provide any image name and will get the url
+  async function getDefaultImageURL(path, imageName) {
+    return await getDownloadURL(ref(storage, `${path}/${imageName}`));
+  }
+
+  //upload image to users specified collection with the specified file name
+  async function uploadImageFile(file, fileName) {
+    const path = `users/${currentUser.uid}/images`;
+    const storageRef = ref(storage, `${path}/${fileName}`);
+    await uploadBytes(storageRef, file);
+    const imageURL = await getDefaultImageURL(path, fileName);
+    updatePortfolio(imageURL, "primaryPhotoURL");
   }
 
   useEffect(() => {
     (async () => {
-      const url = await getDefaultImageURL("default_profile_picture.jpg");
+      const url = await getDefaultImageURL(
+        "default/images",
+        "default_profile_picture.jpg"
+      );
       setDefaultPhotoURL(url);
       setLoading(false);
     })();
   }, []);
 
-  const value = { defaultPhotoURL };
+  const value = { defaultPhotoURL, uploadImageFile };
   return (
     <StorageContext.Provider value={value}>
       {!loading ? (
