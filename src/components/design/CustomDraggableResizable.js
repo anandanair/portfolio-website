3192c1c0a893ff7fdfd9bc20cbf214e74e232d0b
Anand <anandanairaan@gmail.com>
2023-01-17 12:49:56 +0800

    added spacing

--- src/components/design/CustomDraggableResizable.js
+++ src/components/design/CustomDraggableResizable.js
@@ -13,6 +13,16 @@ export default function CustomDraggableResizable(props) {
     top: 0,
     right: 0,
   });
+  const [spacing, setSpacing] = useState({
+    left: 0,
+    right: 0,
+    top: 0,
+  });
+  const [lefLineTop, setLeftLineTop] = useState(0);
+  const [topLineLeft, setTopLineLeft] = useState(0);
+  const [rightLineLeft, setRightLineLeft] = useState(0);
+
+  const [isDragging, setIsDragging] = useState(false);
 
   const handleResizeStart = (event) => {
     event.stopPropagation();
@@ -30,6 +40,29 @@ export default function CustomDraggableResizable(props) {
   const handleResizeStop = (e, direction, ref, d) => {
     setChildWidth(childWidth + d.width);
     props.onResizeStop(d, `${props.id}`);
+  };
+
+  const handleDragStart = () => {
+    setIsDragging(true);
+  };
+
+  const handleDragStop = (event, data) => {
+    setIsDragging(false);
+    props.onDragStop(data, `${props.id}`);
+  };
+
+  const handleDrag = (event, data) => {
+    const left = data.x;
+    const right = parentWidth - (data.x + childWidth);
+    const top = data.y;
+    setLeftLineTop(top - 10 + props.dimensions.height / 2);
+    setTopLineLeft(left - 10 + props.dimensions.width / 2);
+    setRightLineLeft(left + props.dimensions.width);
+    setSpacing({
+      left: left,
+      right: right,
+      top: top,
+    });
   };
 
   //Call once on initialization
@@ -56,33 +89,73 @@ export default function CustomDraggableResizable(props) {
   }, [childWidth, parentWidth]);
 
   return (
-    <Draggable
-      nodeRef={nodeRef}
-      // bounds="parent"
-      bounds={bounds}
-      onStop={(event, data) => props.onDragStop(data, `${props.id}`)}
-      position={props.position}
-      grid={[5, 5]}
-    >
-      <Box
-        ref={nodeRef}
-        sx={{
-          height: props.dimensions.height + "px",
-          width: props.dimensions.width + "px",
-          cursor: "pointer",
-          position: "absolute",
-        }}
+    <div>
+      {isDragging && (
+        <React.Fragment>
+          <div
+            className="horizontal-dotted-line"
+            style={{
+              top: `${lefLineTop}px`,
+              width: spacing.left,
+              color: "white",
+            }}
+          >
+            {spacing.left}px
+          </div>
+          <div
+            className="vertical-dotted-line"
+            style={{
+              left: `${topLineLeft}px`,
+              height: spacing.top,
+              color: "white",
+            }}
+          >
+            {spacing.top}px
+          </div>
+          <div
+            className="horizontal-dotted-line"
+            style={{
+              top: `${lefLineTop}px`,
+              left: `${rightLineLeft}px`,
+              right: 0,
+              width: spacing.right,
+              color: "white",
+            }}
+          >
+            {spacing.right}px
+          </div>
+        </React.Fragment>
+      )}
+      <Draggable
+        nodeRef={nodeRef}
+        // bounds="parent"
+        bounds={bounds}
+        onStart={handleDragStart}
+        onStop={handleDragStop}
+        onDrag={handleDrag}
+        position={props.position}
+        grid={[5, 5]}
       >
-        <Resizable
-          className="textContent"
-          size={props.dimensions}
-          style={props.component === "image" && imageStyleProps}
-          onResizeStop={handleResizeStop}
-          onResizeStart={handleResizeStart}
+        <Box
+          ref={nodeRef}
+          sx={{
+            height: props.dimensions.height + "px",
+            width: props.dimensions.width + "px",
+            cursor: "pointer",
+            position: "absolute",
+          }}
         >
-          {props.children}
-        </Resizable>
-      </Box>
-    </Draggable>
+          <Resizable
+            className="textContent"
+            size={props.dimensions}
+            style={props.component === "image" && imageStyleProps}
+            onResizeStop={handleResizeStop}
+            onResizeStart={handleResizeStart}
+          >
+            {props.children}
+          </Resizable>
+        </Box>
+      </Draggable>
+    </div>
   );
 }
