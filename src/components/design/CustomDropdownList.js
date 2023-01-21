fe4b12d6ea33cde6294cba65aac5d6c8f8645cca
Anand <anandanairaan@gmail.com>
2023-01-21 18:58:41 +0800

    add dialog for text selection

--- src/components/design/CustomDropdownList.js
+++ src/components/design/CustomDropdownList.js
@@ -7,46 +7,78 @@ import {
 } from "@mui/icons-material";
 import {
   Box,
+  Dialog,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
   List,
   ListItem,
   ListItemButton,
   ListItemIcon,
   ListItemText,
 } from "@mui/material";
-import React from "react";
-
-const menuList = [
-  { icon: Title, label: "Add Text" },
-  { icon: Image, label: "Add Image" },
-  { icon: Rectangle, label: "Add Rectangle" },
-  { icon: Circle, label: "Add Cirlce" },
-  { icon: HorizontalRule, label: "Add Line" },
-];
+import { Stack } from "@mui/system";
+import React, { useState } from "react";
 
 export default function CustomDropdownList() {
+  // State
+  const [open, setOpen] = useState(false);
+
+  //Functions
+  const openTextDialog = () => {
+    setOpen(true);
+  };
+
+  const closeTextDialog = () => {
+    setOpen(false);
+  };
+
+  // List of Items in Menu
+  const menuList = [
+    { icon: Title, label: "Add Text", onclick: openTextDialog },
+    { icon: Image, label: "Add Image", onclick: openTextDialog },
+    { icon: Rectangle, label: "Add Rectangle", onclick: openTextDialog },
+    { icon: Circle, label: "Add Cirlce", onclick: openTextDialog },
+    { icon: HorizontalRule, label: "Add Line", onclick: openTextDialog },
+  ];
+
   return (
-    <Box
-      className="rightClickMenu"
-      style={{
-        position: "absolute",
-        top: 40,
-        left: 10,
-        zIndex: 1000,
-        width: "200px",
-      }}
-    >
-      <List dense>
-        {menuList.map((menu, index) => (
-          <ListItem key={index} disableGutters>
-            <ListItemButton onClick={() => {}}>
-              <ListItemIcon>
-                <menu.icon />
-              </ListItemIcon>
-              <ListItemText primary={menu.label} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </Box>
+    <React.Fragment>
+      <Box
+        className="rightClickMenu"
+        style={{
+          position: "absolute",
+          top: 40,
+          left: 10,
+          zIndex: 1000,
+          width: "200px",
+        }}
+      >
+        <List dense>
+          {menuList.map((menu, index) => (
+            <ListItem key={index} disableGutters>
+              <ListItemButton onClick={menu.onclick}>
+                <ListItemIcon>
+                  <menu.icon />
+                </ListItemIcon>
+                <ListItemText primary={menu.label} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Box>
+      <Dialog open={open} onClose={closeTextDialog}>
+        <DialogTitle>Text</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Select a text from the details you added or enter a custom text to
+            add.
+          </DialogContentText>
+          <Stack direction="row">
+            
+          </Stack>
+        </DialogContent>
+      </Dialog>
+    </React.Fragment>
   );
 }
